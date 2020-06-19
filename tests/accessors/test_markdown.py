40944ea1f5a7c43a09d20994f4ab53d6dc05ff43
Jacob Williams <jacobaw@gmail.com>
2020-06-19 08:35:59 -0700

    test parsing of title from markdown file

--- tests/accessors/test_markdown.py
+++ tests/accessors/test_markdown.py
@@ -1,4 +1,5 @@
-from notesdir.accessors.markdown import extract_meta
+from pathlib import Path
+from notesdir.accessors.markdown import extract_meta, MarkdownAccessor
 
 
 def test_extract_meta_none():
@@ -14,3 +15,20 @@ text: regular
 """
     expected = {'title': 'foo bar', 'otherVal': 19}
     assert extract_meta(doc) == expected
+
+
+def test_parse(fs):
+    doc = """---\n
+title: An Examination of the Navel
+...
+#personal #book-draft
+# Preface: Reasons for #journaling
+
+As I have explained at length in [another note](../Another%20Note.md) and also
+published about online (see [this article](http://example.com/blah) among many others), ...
+"""
+    path = Path('/fakenotes/test.md')
+    fs.create_file(path, contents=doc)
+    info = MarkdownAccessor().parse(path)
+    assert info.path == path
+    assert info.title == 'An Examination of the Navel'
