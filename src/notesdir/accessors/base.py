7d9733cf9d610180685ad81f22db1e2a9deb7bbb
Jacob Williams <jacobaw@gmail.com>
2020-06-21 08:51:59 -0700

    add method to get all refs to a path

--- src/notesdir/accessors/base.py
+++ src/notesdir/accessors/base.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass, field
 from pathlib import Path
-from typing import Any, Optional, List, Set
+from typing import Any, Optional, List, Set, Union
+from urllib.parse import urlparse, ParseResult
 
 
 @dataclass
@@ -9,6 +10,27 @@ class FileInfo:
     refs: Set[str] = field(default_factory=set)
     tags: Set[str] = field(default_factory=set)
     title: Optional[str] = None
+
+    def refs_to_path(self, dest: Path) -> Set[str]:
+        """Returns the subset of self.refs that refer to the specified file.
+
+        This excludes out refs that do not refer to local files, or refer to different
+        local files; but includes refs that refer to the same file despite having a different
+        query string or URL fragment, or being specified via relative paths or a path
+        including symlinks.
+        """
+        dest = dest.resolve()
+        result = set()
+        for ref in self.refs:
+            try:
+                url = urlparse(ref)
+                if (not url.scheme) or (url.scheme == 'file' and url.netloc in ['', 'localhost']):
+                    src = Path(url.path).resolve()
+                    if src == dest:
+                        result.add(ref)
+            except ValueError:
+                pass
+        return result
 
 
 @dataclass
--- tests/accessors/test_base.py
+++ tests/accessors/test_base.py
@@ -1,6 +1,48 @@
 from pathlib import Path
 import pytest
-from notesdir.accessors.base import BaseAccessor, FileEdit
+from notesdir.accessors.base import BaseAccessor, FileEdit, FileInfo
+
+
+def test_refs_to_path_skips_invalid_urls():
+    info = FileInfo(Path('foo'), refs={'file://no['})
+    assert info.refs_to_path(Path('bar')) == set()
+
+
+def test_refs_to_path_skips_non_file_schemes():
+    info = FileInfo(Path('foo'), refs={'http:///bar'})
+    assert info.refs_to_path(Path('/bar')) == set()
+
+
+def test_refs_to_path_skips_non_local_hosts():
+    info = FileInfo(Path('foo'), refs={'file://example.com/bar'})
+    assert info.refs_to_path(Path('/bar')) == set()
+
+
+def test_refs_to_path_matches_absolute_paths():
+    info = FileInfo(Path('foo'), refs={'/bar', 'file:///bar', 'file://localhost/bar'})
+    assert info.refs_to_path(Path('/bar')) == {'/bar', 'file:///bar', 'file://localhost/bar'}
+
+
+def test_refs_to_path_matches_relative_paths():
+    info = FileInfo(Path('foo'), refs={'bar'})
+    assert info.refs_to_path(Path('bar')) == {'bar'}
+
+
+def test_refs_to_path_resolves_symlinks(fs):
+    fs.cwd = '/cwd'
+    fs.create_symlink('/cwd/bar', '/cwd/target')
+    info = FileInfo(Path('foo'), refs={'bar/baz'})
+    assert info.refs_to_path(Path('/cwd/target/baz')) == {'bar/baz'}
+
+
+def test_refs_to_path_only_returns_matches():
+    info = FileInfo(Path('foo'), refs={'bar', 'http://example.com/bar', 'baz'})
+    assert info.refs_to_path(Path('bar')) == {'bar'}
+
+
+def test_refs_to_path_ignores_query_and_fragment():
+    info = FileInfo(Path('foo'), refs={'bar#baz', 'bar?baz'})
+    assert info.refs_to_path(Path('bar')) == {'bar#baz', 'bar?baz'}
 
 
 def test_change_empty():
