d918ac6fb9cbf3182173d058355838cac25d73d9
Jacob Williams <jacobaw@gmail.com>
2020-07-06 09:20:38 -0700

    implement info and referrers for sqlite repo

--- src/notesdir/repos/sqlite.py
+++ src/notesdir/repos/sqlite.py
@@ -1,8 +1,13 @@
+from collections import namedtuple
+from datetime import datetime
+from pathlib import Path
 import sqlite3
+from typing import Optional, Set
+from notesdir.models import FileInfo
 from notesdir.repos.direct import DirectRepo
 
 
-CREATE_SCHEMA_SQL = """
+SQL_CREATE_SCHEMA = """
 CREATE TABLE IF NOT EXISTS files (
     id INTEGER PRIMARY KEY,
     path TEXT NOT NULL,
@@ -11,7 +16,7 @@ CREATE TABLE IF NOT EXISTS files (
     stat_mtime INTEGER,
     stat_size INTEGER,
     title TEXT,
-    creation TEXT
+    created TEXT
 );
 
 CREATE UNIQUE INDEX IF NOT EXISTS files_index_path ON files (path);
@@ -28,7 +33,7 @@ CREATE INDEX IF NOT EXISTS file_tags_index_tag ON file_tags (tag);
 CREATE TABLE IF NOT EXISTS file_refs (
     id INTEGER PRIMARY KEY,
     referrer_id INTEGER NOT NULL,
-    referent_id INTEGER NOT NULL,
+    referent_id INTEGER,
     ref TEXT NOT NULL,
     FOREIGN KEY(referrer_id) REFERENCES files(id),
     FOREIGN KEY(referent_id) REFERENCES files(id)
@@ -39,11 +44,20 @@ CREATE INDEX IF NOT EXISTS file_refs_referrer_id_referent_id ON file_refs (refer
 CREATE INDEX IF NOT EXISTS file_refs_referent_id_referrer_id ON file_refs (referent_id, referrer_id);
 """
 
-CLEAR_SQL = """
+SQL_CLEAR = """
 DELETE FROM files;
 DELETE FROM file_tags;
 DELETE_FROM file_refs;
 """
+
+
+SQL_ALL_FOR_REFRESH = 'SELECT id, path, stat_ctime, stat_mtime, stat_size FROM files'
+SqlAllForRefreshRow = namedtuple('SqlAllForRefreshRow', ['id', 'path', 'stat_ctime', 'stat_mtime', 'stat_size'])
+
+SQL_INSERT_FILE = ('INSERT INTO files (path, existent, stat_ctime, stat_mtime, stat_size, title, created)'
+                   ' VALUES (?, ?, ?, ?, ?, ?, ?)')
+SqlInsertFileRow = namedtuple('SqlInsertFileRow', ['path', 'existent', 'stat_ctime', 'stat_mtime', 'stat_size',
+                                                   'title', 'created'])
 
 
 class SqliteRepo(DirectRepo):
@@ -58,14 +72,89 @@ class SqliteRepo(DirectRepo):
 
     def connect(self):
         self.connection = sqlite3.connect(self.db_path)
-        self.connection.executescript(CREATE_SCHEMA_SQL)
+        self.connection.executescript(SQL_CREATE_SCHEMA)
 
     def refresh(self):
+        cursor = self.connection.cursor()
+        cursor.execute(SQL_ALL_FOR_REFRESH)
+        prior_rows = (SqlAllForRefreshRow(*r) for r in cursor.fetchall())
+        prior_rows_by_path = {r.path: r for r in prior_rows}
+        found_paths = set()
 
-        pass
+        ids_by_path = {}
+        refs_to_add = []
+
+        for path in self._paths():
+            pathstr = str(path)
+            found_paths.add(pathstr)
+            stat = path.stat()
+            row = prior_rows_by_path.get(pathstr)
+            if (row and row.stat_ctime == stat.st_ctime
+                    and row.stat_mtime == stat.st_mtime
+                    and row.stat_size == stat.st_size):
+                continue
+            info = super().info(path)
+            if row:
+                file_id = row.id
+                cursor.execute('DELETE FROM file_tags WHERE file_id = ?', (file_id,))
+                cursor.execute('DELETE FROM file_refs WHERE referrer_id = ?', (file_id,))
+            else:
+                newrow = SqlInsertFileRow(path=pathstr,
+                                          existent=True,
+                                          stat_ctime=stat.st_ctime,
+                                          stat_mtime=stat.st_mtime,
+                                          stat_size=stat.st_size,
+                                          title=info.title,
+                                          created=info.created)
+                cursor.execute(SQL_INSERT_FILE, newrow)
+                file_id = cursor.lastrowid
+            cursor.executemany('INSERT INTO file_tags (file_id, tag) VALUES (?, ?)',
+                               ((file_id, t) for t in info.tags))
+            ids_by_path[pathstr] = file_id
+            for referent_path, refs in info.path_refs().items():
+                refs_to_add.append((file_id, str(referent_path) if referent_path else None, refs))
+
+        for referrer_id, referent_path, refs in refs_to_add:
+            referent_id = None
+            if referent_path:
+                referent_id = ids_by_path.get(referent_path)
+                if not referent_id:
+                    cursor.execute('INSERT INTO files (path, existent) VALUES (?, FALSE)', (referent_path,))
+                    referent_id = cursor.lastrowid
+                    ids_by_path[referent_path] = referent_id
+            cursor.executemany('INSERT INTO file_refs (referrer_id, referent_id, ref)'
+                               ' VALUES (?, ?, ?)',
+                               ((referrer_id, referent_id, ref) for ref in refs))
+
+    def info(self, path: Path) -> Optional[FileInfo]:
+        cursor = self.connection.cursor()
+        cursor.execute('SELECT id, title, created FROM files WHERE path = ? AND existent = TRUE',
+                       (str(path.resolve()),))
+        file_row = cursor.fetchone()
+        if not file_row:
+            return None
+        info = FileInfo(path)
+        info.title = file_row[1]
+        info.created = file_row[2] and datetime.fromisoformat(file_row[2])
+        cursor.execute('SELECT tag FROM file_tags WHERE file_id = ?', (file_row[0],))
+        info.tags = {r[0] for r in cursor.fetchall()}
+        cursor.execute('SELECT ref FROM file_refs WHERE referrer_id = ?', (file_row[0],))
+        info.refs = {r[0] for r in cursor.fetchall()}
+        return info
+
+    def referrers(self, path: Path) -> Set[Path]:
+        cursor = self.connection.cursor()
+        path = path.resolve()
+        cursor.execute('SELECT referrers.path'
+                       ' FROM files referrers'
+                       '  INNER JOIN file_refs ON referrers.id = file_refs.referrer_id'
+                       '  INNER JOIN files referents ON referents.id = file_refs.referent_id'
+                       ' WHERE referents.path = ?',
+                       (str(path),))
+        return {Path(r[0]) for r in cursor.fetchall()}
 
     def clear(self):
-        self.connection.executescript(CLEAR_SQL)
+        self.connection.executescript(SQL_CLEAR)
 
     def close(self):
         self.connection.close()
--- tests/repos/test_sqlite.py
+++ tests/repos/test_sqlite.py
@@ -1,12 +1,53 @@
+from datetime import datetime
+from pathlib import Path
+from notesdir.models import FileInfo
 from notesdir.repos.sqlite import SqliteRepo
 
 
-def config(tmpdir):
-    roots = [tmpdir.mkdir('notes')]
-    cache = tmpdir.join('cache.sqlite3')
-    return {'roots': roots, 'cache': cache}
+CONFIG = {'roots': ['/notes'], 'cache': ':memory:'}
 
 
-def test_init(tmpdir):
-    repo = SqliteRepo(config(tmpdir))
+def test_init(fs):
+    repo = SqliteRepo(CONFIG)
     repo.close()
+
+
+def test_info_unknown(fs):
+    fs.create_file('/notes/one.md', contents='Hello')
+    repo = SqliteRepo(CONFIG)
+    assert repo.info(Path('/notes/two.md')) is None
+
+
+def test_info_and_referrers(fs):
+    doc = """---
+title: A Note
+created: 2012-01-02 03:04:05
+...
+I link to [two](two.md) and [three](../otherdir/three.md#heading) and have #two #tags."""
+    path1 = Path('/notes/dir/one.md')
+    path2 = Path('/notes/otherdir/three.md')
+    path3 = Path('/notes/dir/two.md')
+    fs.create_file(path1, contents=doc)
+    fs.create_file(path2, contents='---\ntitle: Note 3\n...\n')
+    repo = SqliteRepo(CONFIG)
+    assert repo.info(path1) == FileInfo(
+        path1,
+        title='A Note',
+        created=datetime(2012, 1, 2, 3, 4, 5),
+        tags={'tags', 'two'},
+        refs={'two.md', '../otherdir/three.md#heading'})
+    assert repo.info(path2) == FileInfo(path2, title='Note 3')
+    assert repo.info(path3) is None
+
+    assert repo.referrers(path1) == set()
+    assert repo.referrers(path2) == {path1}
+    assert repo.referrers(path3) == {path1}
+
+
+def test_refresh(fs):
+    repo = SqliteRepo(CONFIG)
+    path = Path('/notes/one.md')
+    fs.create_file(path, contents='#hello')
+    assert repo.info(path) is None
+    repo.refresh()
+    assert repo.info(path) == FileInfo(path, tags={'hello'})
