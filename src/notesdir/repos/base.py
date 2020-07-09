cf4bcb294291c590f24f3c672fb50756e53cf2f5
Jacob Williams <jacobaw@gmail.com>
2020-07-09 00:41:39 -0700

    add convenience methods to Repo

--- src/notesdir/repos/base.py
+++ src/notesdir/repos/base.py
@@ -4,7 +4,8 @@ from os import PathLike
 from pathlib import Path
 from typing import Dict, Optional, List, Set, Union
 
-from notesdir.models import FileInfo, FileEditCmd, MoveCmd, FileQuery
+from notesdir.models import FileInfo, FileEditCmd, MoveCmd, FileQuery, SetTitleCmd, SetCreatedCmd, AddTagCmd,\
+    DelTagCmd, ReplaceRefCmd
 
 
 class Repo:
@@ -25,6 +26,26 @@ class Repo:
 
     def close(self):
         pass
+
+    def add_tag(self, path: Union[str, bytes, PathLike], tag: str):
+        """Convenience method equivalent to calling change with one AddTagCmd."""
+        self.change([AddTagCmd(Path(path), tag)])
+
+    def del_tag(self, path: Union[str, bytes, PathLike], tag: str):
+        """Convenience method equivalent to calling change with one DelTagCmd."""
+        self.change([DelTagCmd(Path(path), tag)])
+
+    def set_created(self, path: Union[str, bytes, PathLike], created: datetime):
+        """Convenience method equivalent to calling change with one SetCreatedCmd."""
+        self.change([SetCreatedCmd(Path(path), created)])
+
+    def set_title(self, path: Union[str, bytes, PathLike], title: str):
+        """Convenience method equivalent to calling change with one SetTitleCmd."""
+        self.change([SetTitleCmd(Path(path), title)])
+
+    def replace_ref(self, path: Union[str, bytes, PathLike], original: str, replacement: str):
+        """Convenience method equivalent to calling change with one ReplaceRefCmd."""
+        self.change([ReplaceRefCmd(Path(path), original, replacement)])
 
 
 def group_edits(edits: List[FileEditCmd]) -> List[List[FileEditCmd]]:
